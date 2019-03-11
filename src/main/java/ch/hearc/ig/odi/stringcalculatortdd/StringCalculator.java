637ec92cc7d37dc4e4c071ec827456794d19f951
Majid Fatmia <fatima.majid@he-arc.ch>
2019-03-11 21:13:40 +0100

    Add Import LogManager and Logger

--- src/main/java/ch/hearc/ig/odi/stringcalculatortdd/StringCalculator.java
+++ src/main/java/ch/hearc/ig/odi/stringcalculatortdd/StringCalculator.java
@@ -3,8 +3,11 @@ package ch.hearc.ig.odi.stringcalculatortdd;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class StringCalculator {
+    private static final Logger logger = LogManager.getLogger("StringCalculator");
 
     public StringCalculator() {
     }
